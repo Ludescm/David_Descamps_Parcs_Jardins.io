80f922bc11bb94aac9b09b1cf6b2c047e389dd33
Lucas <lucasdescm@gmail.com>
2023-01-15 17:56:44 +0100

    page complete

--- javascript/index.js
+++ javascript/index.js
@@ -44,3 +44,21 @@ closeButton3.addEventListener("click", function() {
 
 
 
+// Script pour faire remonter en haut de la page
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// Script pour afficher le bouton en fonction de la position de la page
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("btn-top").style.display = "block";
+  } else {
+    document.getElementById("btn-top").style.display = "none";
+  }
+}
+
+
